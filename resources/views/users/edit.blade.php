61d3dccad0107694b0cdaaff89a715d593d8599d
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-01-20 15:39:06 -0300

    PUT resolvido

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,52 +1,58 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head> 
-  <body>
-      
-    <form action="{{ route('alterar_user',['id' => $user->id]) }}" method="POST">
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-        @csrf
-        <label for="">Nome:</label> 
-        <input type="text" name="name" id="name" value="{{$user->name}}"><br>
-        <label for="">Email:</label>
-        <input type="text" name="email" id="email" value="{{$user->email}}"><br>
-        <label for="">Senha:</label> 
-        <input name="password" type="password" value="{{$user->password}}"><br>
-        <input type="submit" onclick="editarId()">Salvar</input>
-        <input type="hidden" id="hidden_id" value="{{$user->id}}">
-    </form>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
 
-    <a href="/users/list/">Voltar</a> 
+<body>
+ 
+
+    @csrf
+    <label for="">Nome:</label>
+    <input type="text" name="name" id="name" value="{{$user->name}}"><br>
+    <label for="">Email:</label>
+    <input type="text" name="email" id="email" value="{{$user->email}}"><br>
+    <label for="">Senha:</label>
+    <input name="password" type="password" value="{{$user->password}}"><br>
+    <input type="submit" onclick="editarId()">Salvar</input>
+    <input type="hidden" id="id-hidden" value="{{$user->id}}"> 
+  <a href="/users/list/">Voltar</a>
 
 
-    </body>
-
-    <script>
-     
+</body>
+<script>
     function editarId() {
-        var valorUrl = $('#hidden_id').val(); 
-        var email = $('#email').val(); 
-        var name = $('#name').val();
+        var id = $("#id-hidden").val()
+        var name = $("#name").val()
+        var email = $("#email").val()
+        var password = $("#password").val()
         $.ajax({
-
             type: "PUT",
-            url: 'http://127.0.0.1:8000/api/users/' +valorUrl,
+            url: 'http://127.0.0.1:8000/api/users/'+ id,
             dataType: 'json',
+            data: {
+                'name': name,
+                'email': email,
+                'password': password,
+            },
+            //headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
             success: function(data) {
-                alert("Edição Correta");
-                location.reload();
+                console.log(data)
+                alert("Usuário editado com sucesso")
             },
             error: function() {
-                alert("Erro ao realizar  requisicao");
+                alert("Erro ao realizar  requisicao")
             }
         });
-    }</script>
+    }
+</script>
+
 </html>
